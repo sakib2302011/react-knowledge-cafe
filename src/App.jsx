69d2236b5f8e6e73a5ac6604f12b9e27f3d376aa
NazmusSakib <n.sakib.sad@gmail.com>
2024-09-06 16:07:07 +0600

    added tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 className='text-6xl text-blue-500 bg-fuchsia-400' >Succesfully added tailwind with react</h1>
+      
+    </>
+  )
+}
+
+export default App
